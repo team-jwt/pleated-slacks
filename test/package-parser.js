4b0e2e150f8d1183c09d86060f54fa7dd06d8892
Wade Armstrong <wade@wadearmstrong.com>
2016-05-25 10:57:05 -0700

    Load and parse package.json for dependencies
    - Load an arbitrary package.json file
    - Return an array of npm module dependencies
    - Includes a test!

--- test/package-parser.js
+++ test/package-parser.js
@@ -36,11 +36,6 @@ describe('package.json parser', () => {
 });
 // Check that we grab the keywords from all of the packages in package.json (#6)-
 
-// Check that we read all of the packages from package.json (#5)
-
-
-// Check that we grab the keywords from all of the packages in package.json (#6)
-
 
 // Check that we grab all of the official repos from Docker (#1)
 /*
