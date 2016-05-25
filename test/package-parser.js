e803e23d3b670f00f9d2caea9d94c755b8af9a88
Wade Armstrong <wade@wadearmstrong.com>
2016-05-25 10:57:05 -0700

    Load and parse package.json for dependencies
    - Load an arbitrary package.json file
    - Return an array of npm module dependencies
    - Includes a test!
    
    Set up testing
    
    Get the testing set-up started.
    - Installed Mocha and Expect to run tests
    - Created a fixture so that we can have a predictable package.json to test with
    - Commented out the package parser test file
    
    Load and parse package.json for dependencies
    
    - Load an arbitrary package.json file
    - Return an array of npm module dependencies
    - Includes a test!
    
    Get npm test up and running
    
    Now that we have a basic test for the parser:
    
    - Created npm test-parser to run that
    - Pointed npm test to npm run test-parser so that we can later && run something
      else
    
    Set up testing
    
    Get the testing set-up started.
    - Installed Mocha and Expect to run tests
    - Created a fixture so that we can have a predictable package.json to test with
    - Commented out the package parser test file
    
    Get npm test up and running
    
    Now that we have a basic test for the parser:
    
    - Created npm test-parser to run that
    - Pointed npm test to npm run test-parser so that we can later && run something
      else
    
    Set up testing
    
    Get the testing set-up started.
    - Installed Mocha and Expect to run tests
    - Created a fixture so that we can have a predictable package.json to test with
    - Commented out the package parser test file
    
    Load and parse package.json for dependencies
    
    - Load an arbitrary package.json file
    - Return an array of npm module dependencies
    - Includes a test!

--- test/package-parser.js
+++ test/package-parser.js
@@ -1,11 +1,38 @@
 'use strict';
 
 const expect = require('expect');
+const fs = require('fs');
 
-// Check that we read all of the packages from package.json (#5)
+// File we're testing
+const parser = require('../lib/package-parser.js')
+let pjDeps = parser.dependencies('./test/fixtures/package-test.json')
+
+// Load fixtures
 
 
-// Check that we grab the keywords from all of the packages in package.json (#6)
+let targetDeps = [ 'mongoose',
+  'redis',
+  'express',
+  'q',
+  'sequelize',
+  'ghost',
+  'cradle',
+  'node-solr-smart-client',
+  'expect',
+  'mocha',
+  'consul'];
+
+describe('package.json parser', () => {
+  // Check that we read all of the packages from package.json (#5)
+  it('should grab a list of all packages', () => {
+    expect(pjDeps).toBeAn(Array);
+    expect(pjDeps.length).toEqual(targetDeps.length);
+    expect(pjDeps).toEqual(targetDeps);
+  });
+
+});
+
+// Check that we grab the keywords from all of the packages in package.json (#6)-
 
 
 // Check that we grab all of the official repos from Docker (#1)
