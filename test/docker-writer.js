42f37deab6ff01662102be1dad84a5958d03adab
Josh Apstein <josh4locations@gmail.com>
2016-05-26 12:28:34 -0700

    added basic node Dockerfile writer and tests

---
+++ test/docker-writer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const expect = require('expect');
+const fs = require('fs');
+const writer = require('./../lib/writeDockerfile.js');
+
+describe('write a dockerfile', () => {
+  // make sure it writes to a file called Dockerfile
+  it('should write Dockerfile', () => {
+    writer.makeNodeServer();
+    expect(fs.existsSync('Dockerfile')).toExist; 
+    expect(fs.readFileSync('Dockerfile').toString()).toEqual(`FROM node:latest \nEXPOSE ${process.env.PORT || 3000}`)
+  });
+});
