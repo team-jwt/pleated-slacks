b79b79bccebeb90e7ee99faaa58dda77db4058ed
Tiffany Wong <tbywong@gmail.com>
2016-06-03 19:59:37 -0700

    completed formatted compose file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import * as parsePackage from './lib/package-parser';
+import * as parsePackage from './lib/parsePackage';
 import * as writeDCompose from './lib/writeDCompose';
 import * as writeDockerfile from './lib/writeDockerfile';
 
 /**
  * need to add flow for calling all functions from above modules
  */
+
+
+/* create formatted links to include in docker-compose.yml */
+const links = writeDCompose.formatLinks(VARIABLE); // => get var from parsePackage
+
+/* create formatted services to include in docker-compose.yml*/
+const services = writeDCompose.formatServices(VARIABLE);
+
+/* create docker-compose content string */
+const composeStr = writeDCompose.createCompose(links, services);
+
+/* write docker-compose.yml file containing composeStr */
+writeDCompose.writeFile(composeStr);
